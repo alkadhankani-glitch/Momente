3359b3f87cac322c95d2e5b9754f333d3494e7bf
alkadhankani-glitch <Alka.Dhankani@gmail.com>
2026-06-29 17:19:43 -0400

    path changed for index import

--- app/raja-anjali/import/page.tsx
+++ app/raja-anjali/import/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { supabase } from '../../../lib/supabaseClient'
+import { supabase } from '../../../../lib/supabaseClient'
 import {useMemo,useState} from 'react'
 import Link from 'next/link'
 
